6a3e15fb8e48408d9725b7763e4b7d2ecf0a6ab8
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-16 00:00:25 +0300

    refactored the solution

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,10 +1,9 @@
 package org.example;
 
+import static org.example.utils.GetFriendsBirthYear.getFriendsBirthYear;
+
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
-        int currentlyYear = 3126;
-        int myFriendBirthDay = currentlyYear - 8;
-        System.out.println("my friend's birth year " + myFriendBirthDay);
+        System.out.println("my friend's birth year " + getFriendsBirthYear(3126));
     }
 }
---
+++ src/main/java/org/example/utils/GetFriendsBirthYear.java
@@ -0,0 +1,7 @@
+package org.example.utils;
+
+public class GetFriendsBirthYear {
+    public static int getFriendsBirthYear(int currentlyYear) {
+        return currentlyYear - 8;
+    }
+}
