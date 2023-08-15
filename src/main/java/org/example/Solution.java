683f131e4c4158cf9489fe06c4860faa13172975
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-15 22:42:42 +0300

    resolve the task

---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        int currentlyYear = 3126;
+        int myFriendBirthDay = currentlyYear - 8;
+        System.out.println("my friend's birth year " + myFriendBirthDay);
+    }
+}
