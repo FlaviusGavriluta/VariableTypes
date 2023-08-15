e9b7089dc06f90975f688b5496ccfaab04a04977
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-16 00:07:38 +0300

    add unit test

--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,7 +1,15 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.example.utils.GetFriendsBirthYear;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
-
+    @Test
+    public void getFriendsBirthYear() {
+        int currentYear = 3126;
+        int expected = 3118;
+        assertEquals(expected, GetFriendsBirthYear.getFriendsBirthYear(currentYear));
+    }
 }
